b7b3ae393d50e7b30634c9528ef4e01478ba24b5
Michał Antonik <xmitonikx@gmail.com>
2021-11-12 17:29:23 +0100

    Change server routes

--- server.js
+++ server.js
@@ -1,56 +1,45 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const User = require('./models/users');
 
 const server = express();
 
-mongoose.connect('mongodb://localhost:27017/korepetycje').then((result) => server.listen(3000));
+mongoose.connect('mongodb://localhost:27017/korepetycje').then(() => server.listen(80));
 
+server.use(express.json());
+server.use(cors({ origin: true }));
 server.use(express.static('public'));
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({ extended: true }));
 
-server.post('/sign_up', (req, res) => {
+server.post('/users', (req, res) => {
+  console.log(req.body);
   const user = new User(req.body);
-  User.find({email: user.email}).then((result) => {
-    if(!result.length) {
-      //const user = new User(req.body);
-      //console.log(user.email);
-      user.save().then((result) => {
-        res.redirect('/');
-      })
-    }
-    else {
-      //console.log("account could not be created");
-      res.send("account could not be created");
-    }
-  });
-})
+  user.save()
+  .then(() => {
+    res.sendStatus(200);
+  })
+  .catch(() => {
+    res.sendStatus(403);
+  })
+});
 
-server.post('/sign_in', (req, res) => {
-  const user = new User(req.body);
-  User.find({email: user.email}).then((result) => {
-    //console.log(user.password, result[0].password);
-    if(user.password == result[0].password) {
-      //console.log(user, result);
-      //res.send("correct password");
-      //res.redirect('/sign_in');
+server.get('/users', (_req, res) => {
+  User.find()
+    .then((result) => {
       res.send(result);
-    }
-    else {
-      //console.log(user, result);
-      res.send("wrong password");
-    }
-  });
-})
-  
-server.get('/sign_in', (req, res) => {
-  User.find().then((result) => {
-    res.send(result);
-  });
-})
+    })
+    .catch(() => {
+      res.sendStatus(403);
+    });
+});
 
-server.get('/users', (req, res) => {
-  User.find().then((result) => {
-    res.send(result);
-  });
+server.get('/user/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(() => {
+      res.sendStatus(403);
+    });
 })
