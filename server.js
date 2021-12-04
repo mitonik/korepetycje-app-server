8ef0b9f4a5d2ee1296ce5b8ddf3df5f73a60a827
Michał Antonik <xmitonikx@gmail.com>
2021-12-04 14:31:16 +0100

    Add HTTPS support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const https = require('https');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -9,7 +10,14 @@ const routes = require('./routes/routes');
 const DB_HOST = process.env.DB_HOST;
 const PORT = process.env.PORT;
 
+const options = {
+  cert: process.env.CERT,
+  key: process.env.KEY
+}
+
 const server = express();
+
+const serverHttps = https.createServer(options, server);
 
 server.use(express.json());
 server.use(cors({ origin: true, credentials: true }));
@@ -19,7 +27,7 @@ server.use(cookieParser());
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    server.listen(PORT);
+    serverHttps.listen(PORT);
     console.log('Server listening on port', PORT);
   })
   .catch(err => {
